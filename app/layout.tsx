1c675e27e1cb5f1ecba498d536269fca6667c884
Fatih Yuksel <fatihyuksel3109@gmail.com>
2025-02-15 18:00:30 +0300

    errors fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
-          attribute="data-theme"
+          attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,8 +2,11 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps as NextThemeProviderProps } from "next-themes"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+} & Omit<NextThemeProviderProps, "children">
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
